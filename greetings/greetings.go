81a76bdf280c49f899a04ab03fa6068e7f96c0fa
remain <remain.guo@qq.com>
2023-07-26 16:29:43 +0800

    更改Hello函数，使其无法通过测试

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -14,7 +14,8 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprint(randomFormat())
 	return message, nil
 }
 
