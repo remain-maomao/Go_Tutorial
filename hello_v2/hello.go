fd85cf4bb61b9a5ce17cae1853cf0818bd005153
remain <remain.guo@qq.com>
2023-07-26 16:00:42 +0800

    在greetings模块的greetings包中添加函数Hellos，返回字典

--- hello_v2/hello.go
+++ hello_v2/hello.go
@@ -13,15 +13,16 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Get a greeting message and print it.
-	message, err := greetings.Hello("remain")
-	// If an error was returned, print it to the console and
-	// exit the program
+	// A slice of names.
+	names := []string{"remain", "coco", "corn"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console
+	fmt.Println(messages)
 }
